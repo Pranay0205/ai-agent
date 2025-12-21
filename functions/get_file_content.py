d80e69d8da91ff1e595744559f122cf2ff07afb9
Pranay0205 <pranayghuge@gmail.com>
2025-12-21 13:02:10 -0500

    feat: add file content retrieval and directory listing functions with tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,34 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path) -> str:
+
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+
+        absolute_file_path = os.path.normpath(
+            os.path.join(working_dir_abs, file_path))
+
+        is_file_within_scope = os.path.commonpath(
+            [working_dir_abs, absolute_file_path]) == working_dir_abs
+
+        if not is_file_within_scope:
+            raise RuntimeError(
+                f'Cannot list "{file_path}" as it is outside the permitted working directory')
+
+        if not os.path.isfile(absolute_file_path):
+            raise RuntimeError(
+                f'File not found or is not a regular file: "{file_path}"')
+
+        with open(absolute_file_path, "r") as f:
+            file_content = f.read(MAX_CHARS)
+
+            if f.read(1):
+                file_content += f' [...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content
+
+    except RuntimeError as err:
+        return f"Error: {err}"
---
+++ functions/get_files_info.py
@@ -0,0 +1,34 @@
+import os
+
+
+def get_files_info(working_directory, directory=".") -> str:
+
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        valid_target_dir = os.path.commonpath(
+            [working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            raise RuntimeError(
+                f'Cannot list "{directory}" as it is outside the permitted working directory')
+
+        if not directory:
+            raise RuntimeError(
+                f'"{directory}" is not a directory')
+
+        files_info = []
+        with os.scandir(target_dir) as entries:
+            for entry in entries:
+                file_name = entry.name
+                file_size = entry.stat().st_size
+                is_dir = entry.is_dir()
+                files_info.append(
+                    f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
+
+        return "\n".join(files_info)
+
+    except RuntimeError as err:
+        return (f"Error: {err}\n")
