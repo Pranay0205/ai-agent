953c3b7e0df6cb1644dab870bc458ca227277afa
Pranay0205 <pranayghuge@gmail.com>
2026-01-01 08:48:20 -0500

    feat: implement file listing functionality and enhance system prompt for AI agent

--- main.py
+++ main.py
@@ -1,67 +1,58 @@
 import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from prompts import system_prompt
 
-from config import MODEL_NAME
+from call_functions import available_functions
+from prompts import system_prompt
 
 
 def main():
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="Chatbot")
-
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str,
+                        help="Prompt to send to Gemini")
     parser.add_argument("--verbose", action="store_true",
                         help="Enable verbose output")
-
     args = parser.parse_args()
-
-    if args.user_prompt is None:
-        raise RuntimeError("User prompt is required.")
-
-    messages = [types.Content(
-        role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    # Load API key from environment variable
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    if api_key is None:
-        raise RuntimeError(
-            "gemini api key not found in environment variables.")
-
-    # Initialize Gemini client
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(
+        role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
 
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model=MODEL_NAME,
+        model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
-    if response is None:
-        raise RuntimeError("No response from Gemini API.")
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if response.text is None:
-        raise RuntimeError("No text in response from Gemini API.")
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
 
-    if bool(args.verbose):
-
-        print("User prompt: " + str(messages) + "\n")
-
-        if response.usage_metadata is None:
-            raise RuntimeError(
-                "No usage metadata in response from Gemini API.")
-
-        print(
-            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-
-    print("Response:\n")
-    print(response.text)
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 
 if __name__ == "__main__":
